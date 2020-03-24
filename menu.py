675033e388cbe857474c575d7b875f02a2d0ec06
Manish Shah <45564404+ManishShah120@users.noreply.github.com>
2020-03-24 02:37:17 -0700

    Second Commit

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import os
+import sys
 os.system("tput setaf 1")
 print("\t\t\t\t\t-:Hey wellcome to my TUI that makes life simple:-")
 os.system("tput setaf 7")
@@ -15,6 +16,32 @@ print("""
 """)
 
 ch = input("Enter your CHOICE: ")
-int(ch)
-print(ch)
-
+ch = int(ch)
+if ch == 1:
+	os.system("date")
+elif ch == 2:
+	os.system("cal")
+elif ch == 3:
+	print("Configuring web server...")
+	os.system("sudo apt install apache2")
+elif ch == 4:
+	create_user = input("User Name: ")
+	os.system("sudo useradd {}".format(create_user))
+	print("creating User...")
+	print("Do you want to set the password for ",create_user," now:(y/n)")
+	y_n = input()
+	if y_n == 'y':
+		os.system("passwd {}".format(create_user))
+	else:
+		exit()
+elif ch == 5:
+	file_name = input("Enter the file name with extension: ")
+	os.system("gedit {}".format(file_name))
+	print("Creating ",file_name," ...")
+elif ch == 6:
+	printf("Setting up Network...")
+elif ch == 7:
+	exit()
+else:
+	print("Please Enter a valid choice...")
+	os.system("exit")
